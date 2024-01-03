566a7376a8e8a98d1e73c890cdf79f6fb6ad2c4b
lzx12345236 <149501383+lzx12345236@users.noreply.github.com>
2024-01-03 16:40:22 +0800

    Update limb.py

--- configs/posec3d/slowonly_r50_ntu60_xsub/limb.py
+++ configs/posec3d/slowonly_r50_ntu60_xsub/limb.py
@@ -80,6 +80,6 @@ lr_config = dict(policy='CosineAnnealing', by_epoch=False, min_lr=0)
 total_epochs = 300
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5))
-log_config = dict(interval=194, hooks=[dict(type='TextLoggerHook')])
+log_config = dict(interval=108, hooks=[dict(type='TextLoggerHook')])
 log_level = 'INFO'
 work_dir = './work_dirs/posec3d/slowonly_r50_ntu60_xsub/limb'
