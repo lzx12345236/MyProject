42761d5f659a731b2504b3d4e032287ce04561f1
lzx12345236 <149501383+lzx12345236@users.noreply.github.com>
2024-01-03 20:17:48 +0800

    Update joint.py

--- configs/posec3d/slowonly_r50_ntu60_xsub/joint.py
+++ configs/posec3d/slowonly_r50_ntu60_xsub/joint.py
@@ -74,6 +74,6 @@ lr_config = dict(policy='CosineAnnealing', by_epoch=False, min_lr=0)
 total_epochs = 300
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5))
-log_config = dict(interval=243, hooks=[dict(type='TextLoggerHook')])
+log_config = dict(interval=122, hooks=[dict(type='TextLoggerHook')])
 log_level = 'INFO'
 work_dir = './work_dirs/posec3d/slowonly_r50_ntu60_xsub/joint'
