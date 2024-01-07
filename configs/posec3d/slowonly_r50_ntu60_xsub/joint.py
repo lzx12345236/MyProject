96229aa768b8992bded00bae39737256015e517b
lzx12345236 <149501383+lzx12345236@users.noreply.github.com>
2024-01-07 16:41:07 +0800

    Update joint.py

--- configs/posec3d/slowonly_r50_ntu60_xsub/joint.py
+++ configs/posec3d/slowonly_r50_ntu60_xsub/joint.py
@@ -24,7 +24,7 @@ ann_file = 'data/nturgbd/ntu60_hrnet.pkl'
 left_kp = [1, 3, 5, 7, 9, 11, 13, 15]
 right_kp = [2, 4, 6, 8, 10, 12, 14, 16]
 train_pipeline = [
-    dict(type='UniformSampleFrames', clip_len=201),
+    dict(type='UniformSampleFrames', clip_len=175),
     dict(type='PoseDecode'),
     dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
     dict(type='Resize', scale=(-1, 64)),
@@ -37,7 +37,7 @@ train_pipeline = [
     dict(type='ToTensor', keys=['imgs', 'label'])
 ]
 val_pipeline = [
-    dict(type='UniformSampleFrames', clip_len=201, num_clips=1),
+    dict(type='UniformSampleFrames', clip_len=175, num_clips=1),
     dict(type='PoseDecode'),
     dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
     dict(type='Resize', scale=(64, 64), keep_ratio=False),
